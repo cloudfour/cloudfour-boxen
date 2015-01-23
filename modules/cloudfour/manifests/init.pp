53137cb7ad2678a995cd00fef9bcf96df3a16af4
Lyza Danger Gardner <lyza@lyza.com>
2015-01-23 13:47:07 -0800

    Divide and conquer. Prepping to strip some stuff out of the cloudfour modules

--- modules/cloudfour/manifests/init.pp
+++ modules/cloudfour/manifests/init.pp
@@ -7,37 +7,54 @@ class cloudfour {
 
   include java
   include wget
-  # include nvm
+
+  # everyone uses dropbox
+  include dropbox
+
+  # everyone uses onepassword
+  # we may move away from managing with boxen
+  # at some point
+  include onepassword
 
   include harvest
-  include onepassword
+
+  # everyone uses browsers
+  # this is just to get 'em installed
+  # don't care if boxen manages these later
+  include chrome
+  include firefox
+
+
+  # particular
   include sublime_text
   include iterm2::stable
-  include dropbox
+  include atom
+
+  # dev
   include heroku
   include virtualbox
 
   # https://github.com/boxen/puppet-heroku
+  # goes with heroku
   heroku::plugin { 'accounts':
     source => 'ddollar/heroku-accounts'
   }
 
-  include chrome
-  include firefox
-  include atom
-
+  # Everyone gets Sass
   ruby_gem { 'SASS for all rubies':
     gem          => 'sass',
     version      => '~> 3.1',
     ruby_version => '*',
   }
 
+  # Everyone gets compass
   ruby_gem { 'Compass for all rubies':
     gem          => 'compass',
     version      => '~> 0.12',
     ruby_version => '*',
   }
 
+  # Set system global version defaults
   # Config should be set in hiera
   # See https://github.com/cloudfour/cloudfour-boxen/issues/10
   class { 'nodejs::global': version => 'v0.10' }
