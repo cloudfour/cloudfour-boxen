0467394a3f4b6e6c1e4a3ce5f224c0f1692e3cb1
Lyza Danger Gardner <lyza@lyza.com>
2015-01-27 11:45:54 -0800

    Managing my dotfiles

--- modules/people/manifests/lyzadanger.pp
+++ modules/people/manifests/lyzadanger.pp
@@ -1,4 +1,5 @@
 class people::lyzadanger {
+  $HOME = "/Users/${::boxen_user}"
   include teams::dev
   include iterm2::dev
 
@@ -8,5 +9,26 @@ class people::lyzadanger {
 
   package {
     ['mongodb']:
+  }
+
+  #
+  # DOTFILES
+  #  - install and use homeshick for managing dotfiles
+  #  - install my dotfiles
+  # Inspired by https://github.com/mroth/my-boxen/blob/master/modules/people/manifests/mroth.pp
+  #
+  repository { 'homeshick':
+    source => 'andsens/homeshick',
+    path   => "${HOME}/.homesick/repos/homeshick"
+  }
+  -> repository { 'lyzadanger-dotfiles':
+    source => 'https://github.com/lyzadanger/dotfiles.git',
+    path   => "${HOME}/.homesick/repos/dotfiles",
+    notify => Exec['link-dotfiles']
+  }
+
+  exec { 'link-dotfiles':
+    command     => "bash -c 'source ${HOME}/.homesick/repos/homeshick/homeshick.sh; homeshick link dotfiles --force'",
+    refreshonly => true
   }
 }
