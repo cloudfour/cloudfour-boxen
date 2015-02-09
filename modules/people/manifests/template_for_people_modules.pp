dd48069004d1e3450a5693b5837d158c143b3909
Lyza Danger Gardner <lyza@lyza.com>
2015-02-09 08:49:29 -0800

    Adding people module for Sara on master to expedite

--- modules/people/manifests/template_for_people_modules.pp
+++ modules/people/manifests/template_for_people_modules.pp
@@ -1,6 +1,7 @@
 # Copy to [your-github-username].pp
 # Rename the class to
 # people::[your-github-username]
+# See also: https://github.com/cloudfour/cloudfour-boxen/wiki/Personal-Module-Recipes
 class people::template_for_people_modules {
   ## Uncomment any of the following for the puppet-* module
   #
