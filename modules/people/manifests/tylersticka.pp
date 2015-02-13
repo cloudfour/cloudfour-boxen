8bae6094323644e6f5d726a240315c56025d6f63
Tyler Sticka <tyler@tylersticka.com>
2015-02-13 10:17:47 -0800

    WIP tylersticka/dotfiles

--- modules/people/manifests/tylersticka.pp
+++ modules/people/manifests/tylersticka.pp
@@ -7,6 +7,35 @@ class people::tylersticka {
   include imagealpha
   include licecap
 
+  #
+  # DOTFILES
+  #  - install and use homeshick for managing dotfiles
+  #  - install my dotfiles
+  # Inspired by https://github.com/mroth/my-boxen/blob/master/modules/people/manifests/mroth.pp
+  #
+  file { 'homeshickdir':
+    path => "${HOME}/.homesick/repos",
+    ensure => directory
+  }
+
+  repository { 'homeshick':
+    source => 'andsens/homeshick',
+    path   => "${HOME}/.homesick/repos/homeshick",
+    require => File['homeshickdir']
+  }
+  repository { 'my-dotfiles':
+    ensure  => 'origin/HEAD', # https://github.com/boxen/puppet-repository/issues/12
+    source  => 'tylersticka/dotfiles', # Change me
+    path    => "${HOME}/.homesick/repos/dotfiles",
+    require => File['homeshickdir'],
+    notify  => Exec['link-dotfiles']
+  }
+
+  exec { 'link-dotfiles':
+    command     => "bash -c 'source ${HOME}/.homesick/repos/homeshick/homeshick.sh; homeshick link dotfiles --force'",
+    #refreshonly => true
+  }
+
   # hello
   notice('Hi, Tyler! Happy designing 😄')
 }
