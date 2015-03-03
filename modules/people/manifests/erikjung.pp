34b0c85682f6214565359c7fc47f06f23a17e03f
Erik Jung <erik@cloudfour.com>
2015-03-03 15:38:29 -0800

    Add PhantomJS to my module

--- modules/people/manifests/erikjung.pp
+++ modules/people/manifests/erikjung.pp
@@ -1,9 +1,13 @@
 class people::erikjung {
   include teams::dev
+  include phantomjs::1_9_0
 
   package {
     'pandoc': ensure => installed
   }
+
+  # Set the global version of PhantomJS (version should be installed already)
+  phantomjs::global { '1.9.0': }
 
   notice('Hullo, Erik, thanks for making coffee!')
 }
