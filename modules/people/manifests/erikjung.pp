c00b1ed8a0ea665db705ca941e10ac3a1f16ce90
Erik Jung <erik@cloudfour.com>
2015-03-16 13:40:13 -0700

    Add Tree formula for myself

--- modules/people/manifests/erikjung.pp
+++ modules/people/manifests/erikjung.pp
@@ -4,12 +4,18 @@ class people::erikjung {
   include imageoptim
   include licecap
 
-  package {
-    'pandoc': ensure => installed
+  package { 'pandoc':
+    ensure => installed
+  }
+
+  package { 'tree':
+    ensure => installed
   }
 
   # Set the global version of PhantomJS (version should be installed already)
   phantomjs::global { '1.9.0': }
+
+  nodejs::module { 'divshot for evaluation for 0.10': module => 'divshot-cli', node_version => 'v0.10' }
 
   notice('Hullo, Erik, thanks for making coffee!')
 }
