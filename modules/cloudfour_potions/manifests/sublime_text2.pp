8b7fdd0fec43a2597065e513a3593bc095f5df66
Erik Jung <erik@cloudfour.com>
2015-03-27 10:56:20 -0700

    Add Sublime Packages for CSScomb and JSCS

--- modules/cloudfour_potions/manifests/sublime_text2.pp
+++ modules/cloudfour_potions/manifests/sublime_text2.pp
@@ -1,6 +1,20 @@
 class cloudfour_potions::sublime_text2 {
   include sublime_text::v2
+  nodejs::module { 'jscs for 0.10': module => 'jscs', node_version => 'v0.10' }
+
   sublime_text::v2::package { 'editorconfig':
     source => 'sindresorhus/editorconfig-sublime'
+  }
+
+  sublime_text::v2::package { 'csscomb':
+    source => 'csscomb/sublime-csscomb'
+  }
+
+  sublime_text::v2::package { 'SublimeLinter':
+    source => 'SublimeLinter/SublimeLinter3'
+  }
+
+  sublime_text::v2::package { 'Sublime​Linter-jscs':
+    source => 'SublimeLinter/SublimeLinter-jscs'
   }
 }
