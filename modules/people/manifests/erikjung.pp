558adb95b3fa94326713507b17afb5a7f625bfdd
Erik Jung <erikdavidjung@gmail.com>
2015-04-07 12:28:18 -0700

    Added Homeshick/dotfiles to my stuffs

--- modules/people/manifests/erikjung.pp
+++ modules/people/manifests/erikjung.pp
@@ -1,10 +1,14 @@
 class people::erikjung {
+  $HOME = "/Users/${::boxen_user}"
+
   include teams::dev
   include phantomjs::1_9_0
   include cloudfour_potions::atom
   include cloudfour_potions::dotfiles
   include imageoptim
   include licecap
+
+  # Package stuff
 
   package { 'pandoc':
     ensure => installed
@@ -14,10 +18,38 @@ class people::erikjung {
     ensure => installed
   }
 
-  # Set the global version of PhantomJS (version should be installed already)
   phantomjs::global { '1.9.0': }
 
-  nodejs::module { 'divshot for evaluation for 0.10': module => 'divshot-cli', node_version => 'v0.10' }
+  nodejs::module { 'divshot for evaluation for 0.10':
+    module       => 'divshot-cli',
+    node_version => 'v0.10'
+  }
+
+  # Dotfiles
+
+  file { 'homeshickdir':
+    path   => "${HOME}/.homesick/repos",
+    ensure => directory
+  }
+
+  repository { 'homeshick':
+    source  => 'andsens/homeshick',
+    path    => "${HOME}/.homesick/repos/homeshick",
+    require => File['homeshickdir']
+  }
+
+  repository { 'my-dotfiles':
+    ensure  => 'origin/HEAD',
+    source  => 'erikjung/dotfiles',
+    path    => "${HOME}/.homesick/repos/dotfiles",
+    require => File['homeshickdir'],
+    notify  => Exec['link-dotfiles']
+  }
+
+  exec { 'link-dotfiles':
+    command      => "bash -c 'source ${HOME}/.homesick/repos/homeshick/homeshick.sh; homeshick link dotfiles --force'",
+    #refreshonly => true
+  }
 
   notice('Hullo, Erik, thanks for making coffee!')
 }
