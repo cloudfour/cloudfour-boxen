685fd7067e68de397ed7015a528186867a30f781
Lyza Danger Gardner <lyza@lyza.com>
2015-04-24 12:39:13 -0700

    Adding jshint plugins/packages to atom, sublime

--- modules/cloudfour_potions/manifests/sublime_text.pp
+++ modules/cloudfour_potions/manifests/sublime_text.pp
@@ -2,6 +2,7 @@
 class cloudfour_potions::sublime_text {
   include sublime_text
   nodejs::module { 'jscs for 0.10': module => 'jscs', node_version => 'v0.10' }
+  # jshint is installed via the cloudfour module manifest
 
   sublime_text::package { 'editorconfig':
     source => 'sindresorhus/editorconfig-sublime'
@@ -13,6 +14,10 @@ class cloudfour_potions::sublime_text {
 
   sublime_text::package { 'SublimeLinter':
     source => 'SublimeLinter/SublimeLinter3'
+  }
+
+  sublime_text::package { 'Sublime​Linter-jshint':
+    source => 'SublimeLinter/SublimeLinter-jshint'
   }
 
   sublime_text::package { 'Sublime​Linter-jscs':
--- modules/cloudfour_potions/manifests/sublime_text2.pp
+++ modules/cloudfour_potions/manifests/sublime_text2.pp
@@ -1,6 +1,7 @@
 class cloudfour_potions::sublime_text2 {
   include sublime_text::v2
   nodejs::module { 'jscs for 0.10': module => 'jscs', node_version => 'v0.10' }
+  # jshint gets installed by cloudfour module manifest
 
   sublime_text::v2::package { 'editorconfig':
     source => 'sindresorhus/editorconfig-sublime'
@@ -12,6 +13,10 @@ class cloudfour_potions::sublime_text2 {
 
   sublime_text::v2::package { 'SublimeLinter':
     source => 'SublimeLinter/SublimeLinter3'
+  }
+
+  sublime_text::v2::package { 'Sublime​Linter-jshint':
+    source => 'SublimeLinter/SublimeLinter-jshint'
   }
 
   sublime_text::v2::package { 'Sublime​Linter-jscs':
