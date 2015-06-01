ab94254ea8f464fe1694561205cf5bc47da18614
Lyza Danger Gardner <lyza@lyza.com>
2015-06-01 09:35:06 -0700

    Personal boxen module for GR

---
+++ modules/people/manifests/mrgerardorodriguez.pp
@@ -0,0 +1,14 @@
+# Copy to [your-github-username].pp
+# Rename the class to
+# people::[your-github-username]
+# See also: https://github.com/cloudfour/cloudfour-boxen/wiki/Personal-Module-Recipes
+class people::mrgerardorodriguez {
+
+  include cloudfour_potions::dotfiles
+  include cloudfour_potions::sublime_text
+  include iterm2::dev
+  include teams::dev
+  
+  notice("Hullo, Mr. Gerardo!")
+
+}
