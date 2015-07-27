0381854ccbc72b8718c3d957861619e66cd2f05b
saralohr <sara@cloudfour.com>
2015-07-27 14:30:17 -0700

    Add dotfiles to personal manifest

--- modules/people/manifests/saralohr.pp
+++ modules/people/manifests/saralohr.pp
@@ -5,6 +5,36 @@
 class people::saralohr {
   include cloudfour_potions::dotfiles
   include sublime_text
+  
+  #
+  # DOTFILES
+  #  - install and use homeshick for managing dotfiles
+  #  - install my dotfiles
+  # Inspired by https://github.com/mroth/my-boxen/blob/master/modules/people/manifests/mroth.pp
+  #
+  file { 'homeshickdir':
+    path => "${HOME}/.homesick/repos",
+    ensure => directory
+  }
+
+  repository { 'homeshick':
+    source => 'andsens/homeshick',
+    path   => "${HOME}/.homesick/repos/homeshick",
+    require => File['homeshickdir']
+  }
+  repository { 'my-dotfiles':
+    ensure  => 'origin/HEAD', # https://github.com/boxen/puppet-repository/issues/12
+    source  => 'saralohr/dotfiles', # Change me
+    path    => "${HOME}/.homesick/repos/dotfiles",
+    require => File['homeshickdir'],
+    notify  => Exec['link-dotfiles']
+  }
+
+  exec { 'link-dotfiles':
+    command     => "bash -c 'source ${HOME}/.homesick/repos/homeshick/homeshick.sh; homeshick link dotfiles --force'",
+    #refreshonly => true
+  }
+  
   notice("Hi there, Sara!")
 
 }
