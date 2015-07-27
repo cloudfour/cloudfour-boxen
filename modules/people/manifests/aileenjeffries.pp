61595decb16fa8316dbed6aacd46eb4ae2335fbc
aileenjeffries <aileen@cloudfour.com>
2015-07-27 14:29:25 -0700

    Update aileenjeffries.pp

--- modules/people/manifests/aileenjeffries.pp
+++ modules/people/manifests/aileenjeffries.pp
@@ -1,4 +1,34 @@
 class people::aileenjeffries {
   include cloudfour_potions::dotfiles
+  
+  #
+  # DOTFILES
+  #  - install and use homeshick for managing dotfiles
+  #  - install my dotfiles
+  # Inspired by https://github.com/mroth/my-boxen/blob/master/modules/people/manifests/mroth.pp
+  #
+  file { 'homeshickdir':
+    path => "${HOME}/.homesick/repos",
+    ensure => directory
+  }
+
+  repository { 'homeshick':
+    source => 'andsens/homeshick',
+    path   => "${HOME}/.homesick/repos/homeshick",
+    require => File['homeshickdir']
+  }
+  repository { 'my-dotfiles':
+    ensure  => 'origin/HEAD', # https://github.com/boxen/puppet-repository/issues/12
+    source  => 'aileenjeffries/dotfiles', # Change me
+    path    => "${HOME}/.homesick/repos/dotfiles",
+    require => File['homeshickdir'],
+    notify  => Exec['link-dotfiles']
+  }
+
+  exec { 'link-dotfiles':
+    command     => "bash -c 'source ${HOME}/.homesick/repos/homeshick/homeshick.sh; homeshick link dotfiles --force'",
+    #refreshonly => true
+  }
+
   notice('Hi, there, Aileen! My best to the fam!')
 }
