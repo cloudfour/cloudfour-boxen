0d901055b15fdd2f78e10bb06eed759f2643bfcc
Nicole <nicolemors@gmail.com>
2015-07-27 14:30:31 -0700

    Adding dotfiles to nicolemors manifest

--- modules/people/manifests/nicolemors.pp
+++ modules/people/manifests/nicolemors.pp
@@ -1,5 +1,37 @@
 class people::nicolemors {
   include cloudfour_potions::dotfiles
   include cloudfour_potions::sublime_text2
+  
+  #
+  # DOTFILES
+  #  - install and use homeshick for managing dotfiles
+  #  - install my dotfiles
+  # Inspired by https://github.com/mroth/my-boxen/blob/master/modules/people/manifests/mroth.pp
+  #
+  file { 'homeshickdir':
+    path => "${HOME}/.homesick/repos",
+    ensure => directory
+  }
+
+  repository { 'homeshick':
+    source => 'andsens/homeshick',
+    path   => "${HOME}/.homesick/repos/homeshick",
+    require => File['homeshickdir']
+  }
+  repository { 'my-dotfiles':
+    ensure  => 'origin/HEAD', # https://github.com/boxen/puppet-repository/issues/12
+    source  => 'nicolemors/dotfiles', # Change me
+    path    => "${HOME}/.homesick/repos/dotfiles",
+    require => File['homeshickdir'],
+    notify  => Exec['link-dotfiles']
+  }
+
+  exec { 'link-dotfiles':
+    command     => "bash -c 'source ${HOME}/.homesick/repos/homeshick/homeshick.sh; homeshick link dotfiles --force'",
+    #refreshonly => true
+  }
+
+  # hello
+  
   notice('Hi, Nicole! Here comes some software!')
 }
