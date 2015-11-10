01a57f347d3d3b6e604759373631723a7d08f2ca
Lyza Danger Gardner <lyza@lyza.com>
2015-11-10 11:48:35 -0800

    Update global module installs for 4.0.0

--- modules/cloudfour/manifests/init.pp
+++ modules/cloudfour/manifests/init.pp
@@ -4,12 +4,10 @@ class cloudfour {
 
   # npm
   class { 'nodejs::global': version => '4.0.0' }
-  # npm_module { 'grunt-cli': module => 'grunt-cli', node_version => '0.10' }
-  # npm_module { 'gulp': module => 'gulp', node_version => '0.10' }
-  # npm_module { 'divshot': module => 'divshot-cli', node_version => '0.10' }
-  # npm_module { 'jshint': module => 'jshint', node_version => '0.10' }
-  # npm_module { 'jscs': module => 'jscs', node_version => '0.10' }
-  #nodejs::module { 'yeoman for 0.10': module => 'yo', node_version => 'v0.10' }
+  npm_module { 'gulp': module => 'gulp-cli', node_version => '4.0.0' }
+  npm_module { 'jshint': module => 'jshint', node_version => '4.0.0' }
+  npm_module { 'jscs': module => 'jscs', node_version => '4.0.0' }
+  npm_module { 'yeoman': module => 'yo', node_version => '4.0.0' }
 
   include java
   include wget
