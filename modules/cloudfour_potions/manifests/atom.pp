bfc0add49478ffd3bf6f0fae0c541f830267d673
Lyza Danger Gardner <lyza@lyza.com>
2016-01-08 10:46:30 -0800

    Remove cloudfour_potion::atom

--- modules/cloudfour_potions/manifests/atom.pp
+++
@@ -1,8 +0,0 @@
-class cloudfour_potions::atom {
-  include atom
-  atom::package { 'editorconfig': }
-  atom::package { 'linter': }
-  atom::package { 'linter-jshint': }
-  atom::package { 'linter-jscs': }
-  atom::package { 'css-comb': }
-}
